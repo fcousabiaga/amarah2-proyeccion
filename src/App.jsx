6e487ace079d7bed11d802fbe56c68c577ea0826
fcousabiaga <fcousabiaga@gmail.com>
2026-05-18 20:50:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useRef, useCallback, useEffect } from "react";
+import React, { useState, useMemo, useRef, useCallback, useEffect } from "react";
 
 // ─── PALETA ───────────────────────────────────────────────────────────────────
 const G = {
